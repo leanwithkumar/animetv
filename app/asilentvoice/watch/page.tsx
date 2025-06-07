6be9c1364c87216bb48b4a2ea3e8dc683fbde2ec
leanwithkumar <mrhustle936@gmail.com>
2025-06-07 13:56:06 +0530

    all work done

--- app/asilentvoice/watch/page.tsx
+++ app/asilentvoice/watch/page.tsx
@@ -1,33 +1,14 @@
 'use client'
+import { Suspense } from 'react';
+import Asilentvoicewatch from './watchthis';
 
-import { useSearchParams } from 'next/navigation'
-
-export default function Watch() {
-  const searchParams = useSearchParams()
-  const video = searchParams.get('video')
-
+export default function Page() {
   return (
-    <div className="min-h-screen bg-cover text-white pt-25 px-4 sm:px-8 flex items-start justify-center"
-    style={{
-      backgroundImage : "url('https://images3.alphacoders.com/137/1376355.jpg')"
-    }}>
-      <div className="w-full max-w-6xl">
-        {video ? (
-          <div className="aspect-video w-full  overflow-hidden  border border-white/10 bg-black">
-            <iframe
-              className="w-full h-full"
-              src={video}
-              title="Anime Video"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ) : (
-          <p className="text-center text-red-500 text-lg font-semibold mt-10">
-            No video link provided
-          </p>
-        )}
-      </div>
-    </div>
-  )
+    <>
+    
+    <Suspense fallback={<div className="text-white text-lg p-10">Loading video...</div>}>
+      <Asilentvoicewatch/>
+    </Suspense>
+    </>
+  );
 }
--- app/blackclover/watch/page.tsx
+++ app/blackclover/watch/page.tsx
@@ -1,33 +1,16 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { Suspense } from "react"
+import Blackcloverwatch from "./watchthis"
+
 
 export default function Watch() {
-  const searchParams = useSearchParams()
-  const video = searchParams.get('video')
-
-  return (
-    <div className="min-h-screen bg-cover text-white pt-25 px-4 sm:px-8 flex items-start justify-center"
-    style={{
-      backgroundImage : "url('https://images3.alphacoders.com/137/1376355.jpg')"
-    }}>
-      <div className="w-full max-w-6xl">
-        {video ? (
-          <div className="aspect-video w-full  overflow-hidden  border border-white/10 bg-black">
-            <iframe
-              className="w-full h-full"
-              src={video}
-              title="Anime Video"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ) : (
-          <p className="text-center text-red-500 text-lg font-semibold mt-10">
-            No video link provided
-          </p>
-        )}
-      </div>
-    </div>
-  )
+return(
+  <>
+  <Suspense fallback={<div className="text-white text-lg p-10">Loading video...</div>}>
+        <Blackcloverwatch/>
+  </Suspense>
+  </>
+)
+  
 }
